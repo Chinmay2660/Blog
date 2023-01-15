2ee1865f3e8663524ec8b42babf34e9aa1e7dd97
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-01-15 21:12:41 +0530

    Update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
@@ -28,6 +28,6 @@ app.use(express.static("public"));
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
