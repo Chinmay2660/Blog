5d94452eae3cc9ca7236c50c90004167209de813
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-02-26 20:18:30 +0530

    Add functionality

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require("express"); // require the express module
+const bodyParser = require("body-parser"); // require the body-parser module
+const ejs = require("ejs"); // require the ejs module
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -10,41 +10,58 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+const app = express(); // create a new express app
 
-let posts = [];
+let posts = []; // create an empty array to store posts
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // set the view engine to ejs
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // use the body-parser module
+app.use(express.static("public")); // use the public folder
 
 app.get("/", function (req, res) { // "/" is the home route
-  res.render("home", { startingContent: homeStartingContent, posts: posts });
+  res.render("home", { startingContent: homeStartingContent, posts: posts }); // render the home.ejs file
 });
 
 app.get("/about", function (req, res) { // "/" is the home route
-  res.render("about", { aboutContent: aboutContent });
+  res.render("about", { aboutContent: aboutContent }); // render the about.ejs file
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+app.get("/contact", function (req, res) { // "/" is the home route
+  res.render("contact", { contactContent: contactContent }); // render the contact.ejs file
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose");
+app.get("/compose", function (req, res) { // "/" is the home route
+  res.render("compose"); // render the compose.ejs file
 });
 
-app.post("/compose", function (req, res) {
+app.post("/compose", function (req, res) { // "/" is the home route
   const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
+    title: req.body.postTitle, // get the title from the form
+    content: req.body.postBody // get the content from the form
   };
 
-  posts.push(post);
+  posts.push(post); // push the post to the posts array
 
-  res.redirect("/");
+  res.redirect("/"); // redirect to the home route
 
+});
+
+
+app.get("/posts/:postName", function (req, res) { // "/" is the home route
+  const requestedTitle = req.params.postName; // get the title from the url
+
+  posts.forEach(function (post) { // loop through the posts array
+    const storedTitle = post.title; // get the title from the posts array
+
+    if (storedTitle === requestedTitle) { // if the title from the url matches the title from the posts array
+      console.log("Match found!"); // log "Match found!"
+      // res.render("post", { // render the post.ejs file
+      //   title: post.title, // pass the title to the post.ejs file
+      //   content: post.content // pass the content to the post.ejs file
+      // });
+    }
+  });
 });
 
 app.listen(3000, function () {
